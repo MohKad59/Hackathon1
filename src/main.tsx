37e4913ed018ac33fd1e9c0656485efd4057f115
erwan lorand <erwan.lorand@protonmail.com>
2024-12-19 23:06:03 +0100

    Contact Form

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import Contact from "./components/Contact.tsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <div>Accueil</div>
-      },
-      {
-        path: "/Shopping",
-        element: <div>Shopping</div>
-      },
-      {
-        path: "/Contact",
-        element: <div>Contact</div>
-      },
-    ]
-  },
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <div>Accueil</div>,
+			},
+			{
+				path: "/Shopping",
+				element: <div>Shopping</div>,
+			},
+			{
+				path: "/Contact",
+				element: <Contact />,
+			},
+		],
+	},
 ]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
