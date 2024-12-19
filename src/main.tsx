9c4e26dcaf34dc42ff6d8475059b0fb7e7e984f3
unknown <arthur.saussereau@hotmail.fr>
2024-12-19 15:00:32 +0100

    Initialized react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <div>Accueil</div>
+      },
+      {
+        path: "/Shopping",
+        element: <div>Shopping</div>
+      },
+      {
+        path: "/Contact",
+        element: <div>Contact</div>
+      },
+    ]
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
