f630ed8849ecdf0241aecfa0181696129662a8bc
unknown <arthur.saussereau@hotmail.fr>
2024-12-19 14:44:16 +0100

    created all the files

---
+++ src/components/Accueil.tsx
@@ -0,0 +1,12 @@
+function Accueil() {
+    return (
+        <section id="accueilContainer">
+            <section>
+                <h2></h2>
+                <div><p></p></div>
+            </section>
+        </section>
+    );
+}
+
+export default Accueil;
