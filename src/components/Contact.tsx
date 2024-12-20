afa4ea4fa658c303616d2421d6bf55334a3205ef
erwan lorand <erwan.lorand@protonmail.com>
2024-12-20 12:54:24 +0100

    Lunch Break

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,79 +12,81 @@ function Contact() {
 	return (
 		<div id="contact-container">
 			<div className="top-section">
-				<article className="avatar">
-					{/* <img
-						src="/public/A creative light art piece featuring a Jedi Princess at a computer in a soft, dreamy atmosphere with hazy, soft light, artistic lighting, and abstract patterns.png"
-						alt="Portrait"
-						className="Princesse"
-					/> */}
-				</article>
+				{/* <article className="avatar"> */}
+				<img
+					src="/public/A creative light art piece featuring a Jedi Princess at a computer in a soft, dreamy atmosphere with hazy, soft light, artistic lighting, and abstract patterns.png"
+					alt="Portrait"
+					className="avatar"
+				/>
+				{/* </article>
 				<img src="/envelope-open-regular-24.png" alt="letter-icon" />
-				<img src="/discord-logo-24.png " alt="discord-icon" />
+				<img src="/discord-logo-24.png " alt="discord-icon" /> */}
 			</div>
+			<h2>Formulaire à Potins :</h2>
+			<section className="bottom-section">
+				<div className="Formulaire">
+					<p>Firstname : {firstname}</p>
+					<p>Lastname : {lastname}</p>
 
-			<div className="Formulaire">
-				<h2>Références Galactique</h2>
-				<p>firstname : {firstname}</p>
-				<p>lastname : {lastname}</p>
-
-				<form>
-					<label htmlFor="firstname">
-						firstname:
-						<input
-							type="text"
-							name="firstname"
-							onChange={(event) => setFirstname(event.target.value)}
-						/>
-					</label>
-					<br />
-					<label htmlFor="lastname">
-						lastname
-						<input
-							type="text"
-							name="lastname"
-							onChange={(event) => setLastname(event.target.value)}
-						/>
-					</label>
-					<br />
-					<label htmlFor="planète">
-						planète:
-						<input
-							type="text"
-							name="planète"
-							onChange={(event) => setPlanète(event.target.value)}
-						/>
-					</label>
-					<br />
-					<label htmlFor="espèce">
-						espèce:
-						<input
-							type="text"
-							name="espèce"
-							onChange={(event) => setEspèce(event.target.value)}
-						/>
-					</label>
-					<br />
-					<label htmlFor="spacemail">
-						spacemail:
-						<input
-							type="text"
-							name="spacemail"
-							onChange={(event) => setSpacemail(event.target.value)}
-						/>
-					</label>
-					<br />
-					<label htmlFor="sujet">
-						sujet:
-						<input
-							type="text"
-							name="sujet"
-							onChange={(event) => setSujet(event.target.value)}
-						/>
-					</label>
-					<br />
-				</form>
-			</div>
+					<form>
+						<label htmlFor="firstname">
+							Firstname:
+							<input
+								type="text"
+								name="firstname"
+								onChange={(event) => setFirstname(event.target.value)}
+							/>
+						</label>
+						<br />
+						<label htmlFor="lastname">
+							Lastname
+							<input
+								type="text"
+								name="lastname"
+								onChange={(event) => setLastname(event.target.value)}
+							/>
+						</label>
+						<br />
+						<label htmlFor="planète">
+							Planète:
+							<input
+								type="text"
+								name="planète"
+								onChange={(event) => setPlanète(event.target.value)}
+							/>
+						</label>
+						<br />
+						<label htmlFor="espèce">
+							Espèce:
+							<input
+								type="text"
+								name="espèce"
+								onChange={(event) => setEspèce(event.target.value)}
+							/>
+						</label>
+						<br />
+						<label htmlFor="spacemail">
+							Spacemail:
+							<input
+								type="text"
+								name="spacemail"
+								onChange={(event) => setSpacemail(event.target.value)}
+							/>
+						</label>
+						<br />
+						<label htmlFor="sujet">
+							Sujet:
+							<textarea
+								name="sujet"
+								onChange={(event) => setSujet(event.target.value)}
+								rows="4" /* Le nombre de lignes visibles */
+								cols="50" /* Le nombre de colonnes visibles */
+							/>
+						</label>
+						<br />
+					</form>
+				</div>
+			</section>
 		</div>
 	);
 }
